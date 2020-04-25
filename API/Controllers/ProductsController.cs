d042f4c5caa6693646de2e9da664301c1260f0ed
stephen-talentpay <stephen@talentpay.com>
2020-04-25 16:33:59 +1000

    End of Section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,17 +12,17 @@ namespace Infrastructure.Controllers
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
-    private readonly StoreContext _context;
+    private readonly IProductRepository _repo;
 
-    public ProductsController(StoreContext context)
+    public ProductsController(IProductRepository repo)
     {
-      _context = context;
+      _repo = repo;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = await _context.Products.ToListAsync();
+      var products = await _repo.GetProductsAsync();
 
       return Ok(products);
     }
@@ -29,9 +30,23 @@ namespace Infrastructure.Controllers
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await _context.Products.FindAsync(id);
+      var product = await _repo.GetProductByIdAsync(id);
 
       return Ok(product);
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<List<ProductBrand>>> GetBrands() {
+      var items = await _repo.GetProductBrandsAsync();
+
+      return Ok(items);
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<List<ProductType>>> GetTypes() {
+      var items = await _repo.GetProductTypesAsync();
+
+      return Ok(items);
     }
 
   }
