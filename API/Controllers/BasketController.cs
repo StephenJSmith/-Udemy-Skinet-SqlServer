a140930e003a4bea451cdcfb7b29e4e629f421a7
stephen-talentpay <stephen@talentpay.com>
2020-05-06 07:17:25 +1000

    End of Section 16

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,30 +10,39 @@ namespace API.Controllers
   public class BasketController : BaseApiController
   {
     private readonly IBasketRepository _basketRepository;
-    public BasketController(IBasketRepository basketRepository)
+    private readonly IMapper _mapper;
+
+    public BasketController(
+      IBasketRepository basketRepository,
+    IMapper mapper)
     {
+      _mapper = mapper;
       _basketRepository = basketRepository;
     }
 
     [HttpGet]
-    public async Task<ActionResult<CustomerBasket>> GetBasketById(string id) {
-        var basket = await _basketRepository.GetBasketAsync(id);
+    public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+    {
+      var basket = await _basketRepository.GetBasketAsync(id);
 
-        var returnBasket = basket ?? new CustomerBasket(id);
+      var returnBasket = basket ?? new CustomerBasket(id);
 
-        return returnBasket;
+      return returnBasket;
     }
 
     [HttpPost]
-    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket) {
-        var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basketDto)
+    {
+      var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basketDto);
+      var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
 
-        return Ok(updatedBasket);
+      return Ok(updatedBasket);
     }
 
     [HttpDelete]
-    public async Task DeleteBasketAsync(string id) {
-        await _basketRepository.DeleteBasketAsync(id);
+    public async Task DeleteBasketAsync(string id)
+    {
+      await _basketRepository.DeleteBasketAsync(id);
     }
   }
 }
