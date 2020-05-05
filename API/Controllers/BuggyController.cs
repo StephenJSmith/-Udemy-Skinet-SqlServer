f5f33468180e3041af8d0856680399f6362519f8
stephen-talentpay <stephen@talentpay.com>
2020-05-05 15:46:07 +1000

    End of Section 15

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using API.Errors;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -11,6 +12,12 @@ namespace API.Controllers
     public BuggyController(StoreContext context)
     {
       _context = context;
+    }
+
+    [HttpGet("testauth")]
+    [Authorize]
+    public ActionResult<string> GetSecretText() {
+      return "secret text";
     }
 
     [HttpGet("notfound")]
